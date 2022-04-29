d8c34aaf9c5d71370b6fa4b8083f05bc170a1931
kudamangava <72882706+kudamangava@users.noreply.github.com>
2022-04-29 18:23:30 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import Header from "./components/CalanderHeader/Header";
+import Calander from "./components/Calander/Calander";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Calander />
+      </div>
+    </div>
+  );
+};
+
+export default App;
